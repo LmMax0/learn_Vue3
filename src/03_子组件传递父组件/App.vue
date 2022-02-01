36d3605383341b0f3a077492d4f8ff342be5af03
LmMax0 <3364196039@qq.com>
2022-02-01 15:55:08 +0800

    子组件传递父组件

---
+++ src/03_子组件传递父组件/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h2>当前计数{{ counter }}</h2>
+		<CounterOperation @addN="addNum"  @add="addOne" @sub="subOne"></CounterOperation>
+	</div>
+</template>
+
+<script>
+import CounterOperation from "./CounterOperation.vue"
+export default {
+		components: { 
+			CounterOperation 
+		},
+    data() {
+		return {
+			counter: 0
+		};
+	},
+	methods: {
+		addOne(){
+			this.counter++
+		},
+		subOne(){
+			this.counter--
+		},
+		addNum(value){
+			console.log(typeof(value));
+			console.log(typeof(this.counter));
+			this.counter+=value
+		}
+	},
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/03_子组件传递父组件/CounterOperation.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<button @click="increament">+1</button>
+		<button @click="decreament">-1</button>
+		<br>
+		<input type="number" v-model="this.num">
+		<button @click="increamentN" >+num</button>
+	</div>
+</template>
+
+<script>
+
+export default {
+	// 注册触发事件
+	// emits:["add","sub","addN"],
+	// 对象形式用来进行参数的验证
+	emits:{
+		add:null,
+		sub:null,
+		// addN:payload=>{
+
+		// }
+		addN: (num) =>{
+			console.log("----> "+num);
+			// 通过使用 return true 来通过验证 false 相反
+		}
+	},
+	data() {
+		return {
+			num: 0
+		}
+	},
+	methods: {
+		increament() {
+			console.log("+1")
+			this.$emit("add")
+		},
+		decreament() {
+			console.log("-1")
+			this.$emit("sub")
+		},
+		increamentN(){
+			// console.log(this.num);
+			this.$emit("addN",this.num)
+		}
+	},
+}
+</script>
+   8 89
+<style scoped>
+</style>
